0c3d62d995c5c56d3fc324ca0c8f04d343fa9be3
BlaD200 <blad600@gmail.com>
2021-10-16 16:08:52 +0300

    Initial commit

---
+++ src/test/java/org/vsynytsyn/fidotestbackend/FidoTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vsynytsyn.fidotestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidoTestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
